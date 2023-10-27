acdf0d8f4c995a30efdbd7bcf3caa5151d4725f9
imsi32 <81227251+imsi32@users.noreply.github.com>
2023-10-27 20:31:53 +0300

    Work Structure Added
    Work structure's properties implemented.
    Default and Thing traits are implimented for Work structure.
    Other functions which gets properties not included in Thing trait
    functions implemented.

---
+++ src/work.rs
@@ -0,0 +1,56 @@
+use time::Date;
+use crate::thing::Thing;
+use crate::person::Person;
+
+#[derive(Debug, PartialEq)]
+pub struct Work<'a> {
+    name: String,
+    description: String,
+    interval: (Date, Date),
+    tags: &'a [String],
+    details: &'a [String],
+    persons: &'a [Person<'a>],
+}
+
+impl<'a> Default for Work<'a>{
+    fn default() -> Self {
+        Self {
+            name: String::new(),
+            description: String::new(),
+            interval: (Date::MIN, Date::MAX),
+            tags: &[],
+            details: &[],
+            persons: &[],
+        }
+    }
+}
+
+impl<'a> Thing<'a> for Work<'a>{
+    fn get_name(&self) -> String { self.name.clone() }
+    fn get_description(&self) -> String { self.description.clone() }
+    fn get_interval(&self) -> (Date, Date) { self.interval }
+    fn get_tags(&self) -> &'a [String] { self.tags }
+}
+
+impl<'a> Work<'a> {
+    pub fn get_details(self) -> &'a [String] { self.details }
+    pub fn get_persons(self) -> &'a [Person<'a>] { self.persons }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn work_default() {
+        let event = Work::default();
+        assert_eq!(event, Work{
+            name: String::new(),
+            description: String::new(),
+            interval: (Date::MIN, Date::MAX),
+            tags: &[],
+            details: &[],
+            persons: &[],
+        })
+    }
+}
